7ac307d15ee5d786cf12e824d8174f08f46cf5e1
Setekh <setekh4164@gmail.com>
2026-02-14 11:37:45 +0800

    施加伤害effect

---
+++ QuackCore/BuffSystem/Effects/DamageEffect.cs
@@ -0,0 +1,42 @@
+﻿using Duckov.Buffs;
+
+namespace QuackCore.BuffSystem.Effects
+{
+    /// <summary>
+    /// 伤害效果：使用 DamageInfo 触发受击逻辑
+    /// </summary>
+    public class DamageEffect : IQuackBuffEffect
+    {
+        private readonly float _damage;
+        private readonly bool _ignoreArmor;
+        private readonly DamageTypes _type;
+
+        /// <param name="damage">伤害数值</param>
+        /// <param name="ignoreArmor">是否无视护甲</param>
+        /// <param name="type">伤害类型</param>
+        public DamageEffect(float damage, bool ignoreArmor = true, DamageTypes type = DamageTypes.normal)
+        {
+            _damage = damage;
+            _ignoreArmor = ignoreArmor;
+            _type = type;
+        }
+
+        public void OnApplied(Buff buff, CharacterMainControl target)
+        {
+            if (target == null || target.mainDamageReceiver == null) return;
+
+            DamageInfo info = new DamageInfo(buff.fromWho);
+            info.damageValue = _damage;
+            info.isFromBuffOrEffect = true;
+            info.ignoreArmor = _ignoreArmor;
+            info.ignoreDifficulty = true;
+            info.damageType = _type;
+            info.buff = buff;
+            target.mainDamageReceiver.Hurt(info);
+            
+            ModLogger.LogDebug($"[DamageEffect] 对 {target.name} 造成了 {_damage} 点官方伤害 (忽略护甲: {_ignoreArmor})");
+        }
+
+        public void OnRemoved(Buff buff, CharacterMainControl target) { }
+    }
+}
